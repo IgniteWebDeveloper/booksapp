80efb3d3b849ccead2dff7bd1448d9386dc28dc6
Syed Adnan Ali <61422258+IgniteWebDeveloper@users.noreply.github.com>
2022-03-02 14:23:29 +0530

    commit URL

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, { createContext, useEffect, useState } from 'react'
 
 export const State = createContext(null)
@@ -6,16 +7,18 @@ export const State = createContext(null)
 const Context = (props) => {
     const [books, setbooks] = useState(null)
     useEffect(() => {
-        fetchBooks()
+        getBooks()
     }, [books])
-    
-    const fetchBooks = () => {
-        fetch('https://www.googleapis.com/books/v1/volumes?q=ball:keyes&key=AIzaSyC7YWu0a2xqKnZsRss5ouT8MIsZDQW2-8Y')
-          .then(d => d.json())
-          .then(res => {
-            console.log(res)
-          })
-      }
+
+    const getBooks = () => {
+        axios.get("https://www.googleapis.com/books/v1/volumes?q=flowers&&ball+inauthor:keyes&key=AIzaSyC7YWu0a2xqKnZsRss5ouT8MIsZDQW2-8Y&startIndex=0")
+            .then((response) => {
+                console.log(response.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
     return (
         <State.Provider value={{ books, setbooks }}>
             {props.children}
